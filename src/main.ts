3cdce79eb847dbc454f3e24d156fe0a9d9c8405c
Jakub Szwajka <szwajkajakub@gmail.com>
2023-12-22 13:11:52 +0100

    Remove webpack

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-declare const module: any;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
-
   const config = new DocumentBuilder()
-    .setTitle('Notion proxy api')
-    .setDescription('The notion proxy API description')
-    .setVersion('1.0')
-    .build();
-
-    const document = SwaggerModule.createDocument(app, config);
+  .setTitle('Notion proxy api')
+  .setDescription('The notion proxy API description')
+  .setVersion('1.0')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  
+  await app.listen(3000);
 }
 bootstrap();
