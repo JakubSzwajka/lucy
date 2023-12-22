b80d44c5fe6db94368c8a8b8f717a6c05d802dd9
Jakub Szwajka <szwajkajakub@gmail.com>
2023-12-22 13:14:29 +0100

    Add dotenv

--- src/modules/notion/notion.module.ts
+++ src/modules/notion/notion.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotionService } from './services/notionClient';
 import { NotionController } from './controllers/database.controller';
+import { PagesController } from './controllers/pages.controller';
 
 @Module({
   imports: [],
   controllers: [
-    NotionController
+    NotionController,
+    PagesController,
   ],
   providers: [
     NotionService
