7f23b03fb3d362aec91a3441334ab8295dffd713
Jakub Szwajka <szwajkajakub@gmail.com>
2023-12-22 15:29:20 +0100

    Add servers setting

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
   .setTitle('Notion proxy api')
   .setDescription('The notion proxy API description')
+  .addServer('https://notion-proxy-zeta.vercel.app/api-json')
   .setVersion('1.0')
   .addBearerAuth(
     {
