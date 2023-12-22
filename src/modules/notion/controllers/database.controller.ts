e10289d7a4932913ffe26cc5b21afffa7555ebbf
Jakub Szwajka <szwajkajakub@gmail.com>
2023-12-22 14:05:03 +0100

    Add linting

--- src/modules/notion/controllers/database.controller.ts
+++ src/modules/notion/controllers/database.controller.ts
@@ -1,28 +1,24 @@
-import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 import { NotionService } from "../services/notionClient";
-import { GetDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
-import { AuthGuard } from "../guard/auth.guard";
-import { ApiBasicAuth, ApiBearerAuth } from "@nestjs/swagger";
+import { GetDatabaseResponse, QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { ApiBearerAuth } from "@nestjs/swagger";
 
-
-@Controller('database')
-@ApiBearerAuth('JWT-auth')
+@Controller("database")
+@ApiBearerAuth("JWT-auth")
 export class NotionController {
-    constructor(
-        private readonly notion: NotionService,
-    ) { }
+  constructor(private readonly notion: NotionService) {}
 
-    @Get(":databaseId")
-    public async getDatabase(@Param('databaseId') databaseId: string): Promise<GetDatabaseResponse> {
-        return await this.notion.databases.retrieve({
-            database_id: databaseId,
-        });
-    }
-    
-    @Get(":databaseId/items")
-    public async getDatabaseItems(@Param('databaseId') databaseId: string): Promise<any> {
-        return await this.notion.databases.query({
-            database_id: databaseId,
-        });
-    }
+  @Get(":databaseId")
+  public async getDatabase(@Param("databaseId") databaseId: string): Promise<GetDatabaseResponse> {
+    return await this.notion.databases.retrieve({
+      database_id: databaseId,
+    });
+  }
+
+  @Get(":databaseId/items")
+  public async getDatabaseItems(@Param("databaseId") databaseId: string): Promise<QueryDatabaseResponse> {
+    return await this.notion.databases.query({
+      database_id: databaseId,
+    });
+  }
 }
--- src/modules/notion/controllers/pages.controller.ts
+++ src/modules/notion/controllers/pages.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { NotionService } from "../services/notionClient";
+import { ApiBearerAuth } from "@nestjs/swagger";
+import { GetPageResponse } from "@notionhq/client/build/src/api-endpoints";
 
 
 @Controller('pages')
+@ApiBearerAuth("JWT-auth")
 export class PagesController {
     constructor(
         private readonly notion: NotionService,
         ) { }
 
     @Get(":pageId")
-    public async getPage(@Param('pageId') pageId: string): Promise<any> {
+    public async getPage(@Param('pageId') pageId: string): Promise<GetPageResponse> {
         return await this.notion.pages.retrieve({
             page_id: pageId,
         });
