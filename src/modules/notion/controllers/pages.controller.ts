65e7921c743476cb18ee6efda4b6da8d02d9d6de
Jakub Szwajka <szwajkajakub@gmail.com>
2023-12-22 18:17:51 +0100

    Add page reading with markdown

--- src/modules/notion/controllers/pages.controller.ts
+++ src/modules/notion/controllers/pages.controller.ts
@@ -1,20 +1,19 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { NotionService } from "../services/notionClient";
 import { ApiBearerAuth } from "@nestjs/swagger";
-import { GetPageResponse } from "@notionhq/client/build/src/api-endpoints";
+import { PageBlock } from "../services/blockInterpreter";
 
-
-@Controller('pages')
+@Controller("pages")
 @ApiBearerAuth("JWT-auth")
 export class PagesController {
-    constructor(
-        private readonly notion: NotionService,
-        ) { }
+  constructor(private readonly notion: NotionService,
+    ) {}
 
-    @Get(":pageId")
-    public async getPage(@Param('pageId') pageId: string): Promise<GetPageResponse> {
-        return await this.notion.pages.retrieve({
-            page_id: pageId,
-        });
-    }
+  @Get(":pageId")
+  public async getPage(@Param("pageId") pageId: string): Promise<string> {
+    const page = await this.notion.blocks.children.list({
+        block_id: pageId,
+    });
+    return new PageBlock(page.results).toMarkdown();
+  }
 }
--- src/modules/notion/services/blockInterpreter.ts
+++ src/modules/notion/services/blockInterpreter.ts
@@ -1,152 +1,147 @@
 import { PartialBlockObjectResponse, BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
-type Heading1Block = {
-    type: 'heading_1',
-    heading_1: {
-        rich_text: Array<{
-            type: 'text',
-            text: {
-                content: string,
-            }
-        }>
-    }
+enum BlockType {
+  Text = "text",
+  Mention = "mention",
 }
+
+type BlockObject = {
+  type: BlockType;
+  text: {
+    content: string;
+  };
+  mention: {
+    type: "page";
+    page: {
+      id: string;
+    };
+  };
+  href: string;
+};
+
+type Heading1Block = {
+  type: "heading_1";
+  heading_1: {
+    rich_text: Array<BlockObject>;
+  };
+};
 
 type Heading2Block = {
-    type: 'heading_2',
-    heading_2: {
-        rich_text: Array<{
-            type: 'text',
-            text: {
-                content: string,
-            }
-        }>
-    }
-}
+  type: "heading_2";
+  heading_2: {
+    rich_text: Array<BlockObject>;
+  };
+};
 
 type Heading3Block = {
-    type: 'heading_3',
-    heading_3: {
-        rich_text: Array<{
-            type: 'text',
-            text: {
-                content: string,
-            }
-        }>
-    }
-}
+  type: "heading_3";
+  heading_3: {
+    rich_text: Array<BlockObject>;
+  };
+};
 
 type ParagraphBlock = {
-    type: 'paragraph',
-    paragraph: {
-        rich_text: Array<{
-            type: 'text',
-            text: {
-                content: string,
-            }
-        }>
-    }
-}
+  type: "paragraph";
+  paragraph: {
+    rich_text: Array<BlockObject>;
+  };
+};
 
 type BulletedListItemBlock = {
-    type: 'bulleted_list_item',
-    bulleted_list_item: {
-        rich_text: Array<{
-            type: 'text',
-            text: {
-                content: string,
-            }
-        }>
-    }
-}
+  type: "bulleted_list_item";
+  bulleted_list_item: {
+    rich_text: Array<BlockObject>;
+  };
+};
 
 type Block = Heading1Block | Heading2Block | Heading3Block | ParagraphBlock | BulletedListItemBlock;
 
 export class PageBlock {
-    constructor(
-        public readonly obj: Array<PartialBlockObjectResponse | BlockObjectResponse>, 
-    ) {}
+  constructor(public readonly obj: Array<PartialBlockObjectResponse | BlockObjectResponse>) {}
 
-
-    toMarkdown() {
-        let markdown = '';
-        for (const block of this.obj) {
-            markdown += this.blockToMarkdown(block as Block);
-        }
-        return markdown;
+  toMarkdown() {
+    let markdown = "";
+    for (const block of this.obj) {
+      markdown += this.blockToMarkdown(block as unknown as Block);
     }
+    return markdown;
+  }
 
-    blockToMarkdown(block: Block) {
-        let markdown = '';
-         if (block.type === 'heading_1') {
-            markdown += this.heading1ToMarkdown(block);
-        } else if (block.type === 'heading_2') {
-            markdown += this.heading2ToMarkdown(block);
-        } else if (block.type === 'heading_3') {
-            markdown += this.heading3ToMarkdown(block);
-        }   else if (block.type === 'paragraph') {
-            markdown += this.paragraphToMarkdown(block);
-        } else if (block.type === 'bulleted_list_item') {
-            markdown += this.bulletedListItemToMarkdown(block);
-        }
-            // } else if (block.type === 'numbered_list_item') {
-        //     markdown += this.numberedListItemToMarkdown(block);
-        // } else if (block.type === 'to_do') {
-        //     markdown += this.todoToMarkdown(block);
-        // } else if (block.type === 'toggle') {
-        //     markdown += this.toggleToMarkdown(block);
-        // } else if (block.type === 'child_page') {
-        //     markdown += this.childPageToMarkdown(block);
-        // } else if (block.type === 'unsupported') {
-        //     markdown += this.unsupportedToMarkdown(block);
-        // } else {
-        //     markdown += this.unsupportedToMarkdown(block);
-        // }
-        return markdown;
+  blockToMarkdown(block: Block) {
+    let markdown = "";
+    if (block.type === "heading_1") {
+      markdown += this.heading1ToMarkdown(block);
+    } else if (block.type === "heading_2") {
+      markdown += this.heading2ToMarkdown(block);
+    } else if (block.type === "heading_3") {
+      markdown += this.heading3ToMarkdown(block);
+    } else if (block.type === "paragraph") {
+      markdown += this.paragraphToMarkdown(block);
+    } else if (block.type === "bulleted_list_item") {
+      markdown += this.bulletedListItemToMarkdown(block);
     }
+    // } else if (block.type === 'numbered_list_item') {
+    //     markdown += this.numberedListItemToMarkdown(block);
+    // } else if (block.type === 'to_do') {
+    //     markdown += this.todoToMarkdown(block);
+    // } else if (block.type === 'toggle') {
+    //     markdown += this.toggleToMarkdown(block);
+    // } else if (block.type === 'child_page') {
+    //     markdown += this.childPageToMarkdown(block);
+    // } else if (block.type === 'unsupported') {
+    //     markdown += this.unsupportedToMarkdown(block);
+    // } else {
+    //     markdown += this.unsupportedToMarkdown(block);
+    // }
+    return markdown;
+  }
 
-    heading1ToMarkdown(block: Heading1Block) {
-        let markdown = '';
-        for (const text of block.heading_1.rich_text) {
-            markdown += `# ${text.text.content}`;
-        }
-        markdown += '\n\n';
-        return markdown;
+  heading1ToMarkdown(block: Heading1Block) {
+    let markdown = "";
+    for (const text of block.heading_1.rich_text) {
+      markdown += `# ${text.text.content}`;
     }
+    markdown += "\n\n";
+    return markdown;
+  }
 
-    heading2ToMarkdown(block: Heading2Block) {
-        let markdown = '';
-        for (const text of block.heading_2.rich_text) {
-            markdown += `## ${text.text.content}`;
-        }
-        markdown += '\n\n';
-        return markdown;
+  heading2ToMarkdown(block: Heading2Block) {
+    let markdown = "";
+    for (const text of block.heading_2.rich_text) {
+      markdown += `## ${text.text.content}`;
     }
+    markdown += "\n\n";
+    return markdown;
+  }
 
-    heading3ToMarkdown(block: Heading3Block) {
-        let markdown = '';
-        for (const text of block.heading_3.rich_text) {
-            markdown += `### ${text.text.content}`;
-        }
-        markdown += '\n\n';
-        return markdown;
+  heading3ToMarkdown(block: Heading3Block) {
+    let markdown = "";
+    for (const text of block.heading_3.rich_text) {
+      markdown += `### ${text.text.content}`;
     }
+    markdown += "\n\n";
+    return markdown;
+  }
 
-    paragraphToMarkdown(block: ParagraphBlock) {
-        let markdown = '';
-        for (const text of block.paragraph.rich_text) {
-            markdown += `${text.text.content}`;
-        }
-        markdown += '\n\n';
-        return markdown;
+  paragraphToMarkdown(block: ParagraphBlock) {
+    let markdown = "";
+    for (const text of block.paragraph.rich_text) {
+      markdown += `${text.text.content}`;
     }
+    markdown += "\n\n";
+    return markdown;
+  }
 
-    bulletedListItemToMarkdown(block: BulletedListItemBlock) {
-        let markdown = '';
-        for (const text of block.bulleted_list_item.rich_text) {
-            markdown += `* ${text.text.content}`;
-        }
-        markdown += '\n\n';
-        return markdown;
+  bulletedListItemToMarkdown(block: BulletedListItemBlock) {
+    let markdown = "*";
+    for (const text of block.bulleted_list_item.rich_text) {
+      if (text.type === BlockType.Text) {
+        markdown += text.text.content;
+      } else if (text.type === BlockType.Mention) {
+        markdown += `[page-title](${text.href})`;
+      }
     }
+    markdown += "\n\n";
+    return markdown;
+  }
 }
