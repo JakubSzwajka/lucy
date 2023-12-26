ed6786a8418fe570d81d76bd4bbbd5899e4b02e3
Jakub Szwajka <szwajkajakub@gmail.com>
2023-12-26 23:34:30 +0100

    Add projects endpoint

---
+++ src/modules/notion/controllers/project.controller.ts
@@ -0,0 +1,80 @@
+import { Controller, Get, Param } from "@nestjs/common";
+import { ApiBearerAuth } from "@nestjs/swagger";
+import { NotionService } from "../services/notionClient";
+import { PageBlock } from "../services/blockInterpreter";
+
+
+type ProjectPage = {
+    properties: {
+        Tasks: {
+            relation: {
+                id: string
+            }[]
+        }
+    }
+}
+
+type TaskPage = {
+    properties: {
+        "Task name": {
+            title: {
+                plain_text: string
+            }[]
+        }
+    }
+}
+
+
+@Controller("projects")
+@ApiBearerAuth("JWT-auth")
+export class ProjectsController {
+  constructor(private readonly notion: NotionService) {}
+
+  @Get()
+  public async getProjects() {
+    try {
+      return await this.notion.databases.query({
+        database_id: process.env.PROJECTS_DATABASE_ID as string,
+      });
+    } catch (e) {
+      console.error(e);
+      return e;
+    }
+  }
+
+  @Get(":projectId")
+    public async getProject(@Param("projectId") projectId: string) {
+        try {
+        const project = await this.notion.pages.retrieve({
+            page_id: projectId
+        }) as unknown as ProjectPage;
+        const subTasksIds = project.properties.Tasks.relation.map(task => task.id)
+        const subTasksMarkdown = await Promise.all(subTasksIds.map(async taskId => {
+            const taskPage = await this.notion.pages.retrieve({
+                page_id: taskId
+            }) as unknown as TaskPage;
+            const task = await this.notion.blocks.children.list({
+                block_id: taskId
+            })
+            return {
+                id: taskId,
+                page: taskPage.properties["Task name"].title.map(title => title.plain_text).join(""),
+                markdown: new PageBlock(task.results).toMarkdown()
+            }
+        }))
+
+        const projectPage = await this.notion.blocks.children.list({
+            block_id: projectId
+        })
+        const projectPageMarkdown = new PageBlock(projectPage.results).toMarkdown();
+        return {
+            project,
+            projectPage: projectPageMarkdown,
+            subTasksPages: subTasksMarkdown
+        }
+        } catch (e) {
+        console.error(e);
+        return e;
+        }
+    }
+}
--- src/modules/notion/notion.module.ts
+++ src/modules/notion/notion.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { NotionService } from './services/notionClient';
 import { DatabaseController } from './controllers/database.controller';
 import { PagesController } from './controllers/pages.controller';
+import { ProjectsController } from './controllers/project.controller';
 
 @Module({
   imports: [],
   controllers: [
     DatabaseController,
     PagesController,
+    ProjectsController
   ],
   providers: [
     NotionService
