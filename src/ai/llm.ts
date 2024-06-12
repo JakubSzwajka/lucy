a55c32bcaebe4af1edfd3539bd5acdc307aa2f82
Jakub Szwajka <szwajkajakub@gmail.com>
2024-06-12 14:20:24 +0200

    Add conversation history mechanism

--- src/ai/llm.ts
+++ src/ai/llm.ts
@@ -1,16 +1,31 @@
-import { HumanMessage, SystemMessage } from '@langchain/core/messages';
+import {
+  HumanMessage,
+  SystemMessage,
+  AIMessage,
+} from '@langchain/core/messages';
 import { ChatOpenAI } from '@langchain/openai';
 import { env } from 'src/env';
 import { LucySystemMessage } from './prompt';
+import { Message } from 'src/lucy/entities/message.entity';
 
-export const call = async (message: string): Promise<string> => {
+export const call = async (
+  message: string,
+  conversationHistory: Message[],
+): Promise<string> => {
   const chat = new ChatOpenAI({
     apiKey: env.OPENAI_API_KEY,
     model: 'gpt-3.5-turbo',
   });
 
+  const conversation = conversationHistory
+    .map((message) => {
+      return [new HumanMessage(message.human), new AIMessage(message.agent)];
+    })
+    .flat();
+
   const { content } = await chat.invoke([
     new SystemMessage(LucySystemMessage),
+    ...conversation,
     new HumanMessage(message),
   ]);
   return content as string;
--- src/lucy/lucy.service.ts
+++ src/lucy/lucy.service.ts
@@ -3,7 +3,7 @@ import { call } from 'src/ai';
 import { v4 } from 'uuid';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Message, MessageSource } from './entities/message.entity';
-import { Repository } from 'typeorm';
+import { Repository, Raw } from 'typeorm';
 
 @Injectable()
 export class LucyService {
@@ -12,8 +12,20 @@ export class LucyService {
     private readonly messageRepository: Repository<Message>,
   ) {}
 
+  private readonly MINUTES_OF_CONVERSATION_HISTORY = 10;
+
   async talk(query: string): Promise<string> {
-    const response = await call(query);
+    const history = await this.messageRepository.find({
+      // last 10 minutes
+      where: {
+        createdAt: Raw((alias) => `${alias} > :date`, {
+          date: new Date(
+            Date.now() - 1000 * 60 * this.MINUTES_OF_CONVERSATION_HISTORY,
+          ),
+        }),
+      },
+    });
+    const response = await call(query, history);
     try {
       const message = this.messageRepository.create(
         new Message({
@@ -24,7 +36,6 @@ export class LucyService {
         }),
       );
       await this.messageRepository.save(message);
-      console.log('Message saved', message);
     } catch (error) {
       console.error('Error saving message', error);
     }
