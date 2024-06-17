099372737b099cb677797d14f22ad151c9ca8f5b
Jakub Szwajka <szwajkajakub@gmail.com>
2024-06-17 10:33:01 +0200

    Add react router

---
+++ web/src/components/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1 className="text-3xl font-semibold text-muted-foreground">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
