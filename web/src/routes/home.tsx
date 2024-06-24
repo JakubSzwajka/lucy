85cc7804a6e452bd4380a217289997f45a33dcf8
Jakub Szwajka <szwajkajakub@gmail.com>
2024-06-24 12:07:37 +0200

    Add multiple components

--- web/src/routes/home.tsx
+++ web/src/routes/home.tsx
@@ -8,7 +8,9 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex items-center justify-center h-full">
-      <h1 className="text-3xl font-semibold text-muted-foreground">Home</h1>
+      <h1 className="text-3xl font-semibold text-muted-foreground">
+        Home - hello!
+      </h1>
     </div>
   );
 };
