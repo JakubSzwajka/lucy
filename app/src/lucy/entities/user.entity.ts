b817d1da5b5439c1bb43cf42975d83dee157400c
Jakub Szwajka <szwajkajakub@gmail.com>
2024-06-24 13:17:47 +0200

    Add user model

---
+++ app/src/lucy/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
