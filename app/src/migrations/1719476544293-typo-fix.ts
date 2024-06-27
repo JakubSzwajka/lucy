17a232da6d57010e6fcb0883330bc0380762641d
Jakub Szwajka <szwajkajakub@gmail.com>
2024-06-27 10:27:26 +0200

    Lint

--- app/src/migrations/1719476544293-typo-fix.ts
+++ app/src/migrations/1719476544293-typo-fix.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class TypoFix1719476544293 implements MigrationInterface {
-    name = 'TypoFix1719476544293'
+  name = 'TypoFix1719476544293';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "agent" RENAME COLUMN "prefferedChannel" TO "preferredChannel"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "agent" RENAME COLUMN "prefferedChannel" TO "preferredChannel"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "agent" RENAME COLUMN "preferredChannel" TO "prefferedChannel"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "agent" RENAME COLUMN "preferredChannel" TO "prefferedChannel"`,
+    );
+  }
 }
