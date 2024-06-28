ca80a2a4372b927bf5af697f261597739a7330d1
Jakub Szwajka <szwajkajakub@gmail.com>
2024-06-28 09:40:42 +0200

    Lint

--- app/src/migrations/1718273914153-create-skill-model.ts
+++ app/src/migrations/1718273914153-create-skill-model.ts
@@ -5,7 +5,7 @@ export class CreateSkillModel1718273914153 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "skill" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying NOT NULL, "parameters" text NOT NULL, CONSTRAINT "UQ_0f49a593960360f6f85b692aca8" UNIQUE ("name"), CONSTRAINT "PK_a0d33334424e64fb78dc3ce7196" PRIMARY KEY ("id"))`
+      `CREATE TABLE "skill" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying NOT NULL, "parameters" text NOT NULL, CONSTRAINT "UQ_0f49a593960360f6f85b692aca8" UNIQUE ("name"), CONSTRAINT "PK_a0d33334424e64fb78dc3ce7196" PRIMARY KEY ("id"))`,
     );
   }
 
