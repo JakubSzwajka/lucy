2b90ab99aadc1eb5f5e6b320a1ce4317939f1845
Jakub Szwajka <szwajkajakub@gmail.com>
2024-06-28 09:38:08 +0200

    Fix tests

--- app/src/auth/__test__/user.service.spec.ts
+++ app/src/auth/__test__/user.service.spec.ts
@@ -29,7 +29,7 @@ describe('UserService', () => {
         expect.objectContaining({
           id: expect.any(String),
           email: user.email,
-        })
+        }),
       );
 
       const userFromDb = await service.getUserByEmail(user.email);
@@ -38,7 +38,7 @@ describe('UserService', () => {
         expect.objectContaining({
           id: user.id,
           email: user.email,
-        })
+        }),
       );
     });
 
@@ -52,7 +52,7 @@ describe('UserService', () => {
           email,
           password,
         });
-      } catch (error) {
+      } catch (error: any) {
         expect(error.message).toBe('User with this email already exists');
       }
     });
