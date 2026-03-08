866023acdcc40fb61c429b10d80e9bcdb3ce84ac
Jakub Szwajka <szwajkajakub@gmail.com>
2026-03-08 11:59:41 +0100

    Fix chat panel overflow by adding min-h-0 to flex container

--- agents-webui/src/components/ChatPanel.tsx
+++ agents-webui/src/components/ChatPanel.tsx
@@ -79,7 +79,7 @@ export function ChatPanel() {
   }
 
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col min-h-0">
       <MessageList items={items} />
       <div className="border-t border-border px-4 py-2 flex items-center">
         <ModelSelector models={models} value={selectedModelId} onChange={handleModelChange} />
